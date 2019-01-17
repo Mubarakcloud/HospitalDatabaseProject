bb9dce8b4e1b5fff9f4f0160545e0b70ac631e18
W3ndige <w3ndige@gmail.com>
2019-01-17 20:24:43 +0100

    View functionality of the client, some fixes

--- HospitalViews.sql
+++ HospitalViews.sql
@@ -13,12 +13,12 @@ AS
 SELECT p.imie, p.nazwisko, p.numer_telefonu, o.nazwa_oddzialu FROM Zabiegi z 
     JOIN zab_prac zp ON z.id_zabiegu = zp.id_zabiegu 
     JOIN pracownik p ON p.id_pracownika = zp.id_pracownika
-    JOIN oddzial o ON o.id_oddzialu = z.id_oddzialu
+    JOIN oddzial o ON o.id_oddzialu = p.id_oddzialu
     WHERE z.data_zabiegu = SYSDATE AND p.stanowisko = 'Lekarz';
 
 CREATE OR REPLACE VIEW Zaplanowane_zabiegi
 AS
-SELECT z.data_zabiegu, z.typ_zabiegu, z.spodziewany_czas_trwania, o.nazwa_oddzialu, s.numer_sali FROM Zabiegi z
+SELECT z.data_zabiegu, z.typ_zabiegu, z.spodziewane_zakonczenie, o.nazwa_oddzialu, s.numer_sali FROM Zabiegi z
     JOIN sala s ON s.id_sali = z.sala
     JOIN oddzial o ON o.id_oddzialu = s.id_oddzialu 
     WHERE z.data_zabiegu > SYSDATE;
@@ -29,11 +29,11 @@ SELECT l.nazwa_leku, l.dostepnosc, l.uwagi, p.imie, p.nazwisko, lk.specjalizacja
     JOIN kart_lek kl ON l.id_leku = kl.podany_lek
     JOIN karta_choroby kch ON kch.id_karty = kl.id_karty
     JOIN Lekarz lk ON lk.id_lekarza = kch.lekarz_prowadzacy
-    JOIN Pracownik p ON p.id_pracownika = lk.id_pracownika
+    JOIN Pracownik p ON p.id_pracownika = lk.id_pracownika;
 
 CREATE OR REPLACE VIEW Pacjenci_na_miejscu
 AS
 SELECT p.imie, p.nazwisko, o.nazwa_oddzialu, s.numer_sali FROM Pacjent p
     JOIN karta_choroby kch ON kch.pacjent = p.pesel
     JOIN sala s ON s.id_sali = kch.sala
-    JOIN oddzial o ON o.id_oddzialu = s.id_oddzialu
+    JOIN oddzial o ON o.id_oddzialu = s.id_oddzialu;
---
+++ client/client.py
@@ -0,0 +1,64 @@
+import os
+import cx_Oracle
+from flask import Flask, render_template, request
+
+db_user = 'HR'
+db_password = 'oracle'
+db_connect = '192.168.0.66:1521/ORCL'
+
+app = Flask(__name__)
+
+def prepare_views():
+      connection = cx_Oracle.connect(db_user, db_password, db_connect)
+      view_cursor = connection.cursor()
+      view_cursor.execute('SELECT view_name FROM user_views')
+      views = view_cursor.fetchall()
+      view_cursor.close()
+      connection.close()
+      return views
+
+@app.route('/')
+def index():
+      views = prepare_views()
+      option_views = []
+      for view in views:
+            option_views.append(view[0])
+
+      return render_template('index.html', view_options=views)
+
+@app.route('/views', methods=['POST'])
+def views():
+      if request.method != 'POST':
+            return "Not correct method, use with POST."
+      
+      connection = cx_Oracle.connect(db_user, db_password, db_connect)
+      current_view = request.form.get('view_name')
+      if current_view == None:
+            return "Didn't specify view."
+
+      views = prepare_views()
+      if not any(current_view in view for view in views):
+            return "Non existent view: " + current_view
+
+      cursor = connection.cursor()
+      cursor.execute("SELECT * FROM %s" % current_view) # Is it safe?
+      columns = cursor.fetchall()
+      cursor.close()
+      connection.close()
+      
+      return render_template("view_template.html", 
+                              cols=columns, view=current_view) # Need to escape?
+
+@app.route('/procedures')
+def procedures():
+      connection = cx_Oracle.connect(db_user, db_password, db_connect)
+      current_view = request.args.get('name').upper()
+      if current_view == None:
+            return "Didn't specify view."
+      
+
+      connection.close()
+
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=1337)
