fe0dd7808ef055f58c19b58e4386f063202ea4c8
Eliathen <44061188+Eliathen@users.noreply.github.com>
2019-01-21 11:03:03 +0100

    Add new trigger przy_podaniu_leku

--- HospitalTrigger.sql
+++ HospitalTrigger.sql
@@ -15,4 +15,15 @@ BEGIN
     INSERT INTO karta_choroby VALUES (karta_id_seq.NEXTVAL, :new.PESEL, NULL, NULL, SYSDATE, NULL, NULL, NULL);
 END;
 /
-
+CREATE OR REPLACE TRIGGER przy_podaniu_leku
+BEFORE INSERT ON KART_LEK
+FOR EACH ROW
+DECLARE
+    aktualna_ilosc leki.dostepnosc%TYPE;
+BEGIN
+    SELECT l.dostepnosc INTO aktualna_ilosc FROM LEKI l WHERE l.id_leku = :new.podany_lek; 
+    IF aktualna_ilosc>0 THEN
+        UPDATE LEKI SET DOSTEPNOSC =  DOSTEPNOSC - 1 WHERE id_leku = :new.podany_lek;
+    END IF;
+END;
+/
